cb79fbbbd38f0309d917cc9355701ee6f9f7bc9d
Travier.Thomas <travier.thomas@cci.com>
2024-01-29 14:21:58 +0100

    finalisation des détails par produits avec galerie d'image

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,8 +1,14 @@
+
+
+import React from "react";
+
 import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
+import Gallery from "@/components/gallery";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
-import React from "react";
+import Info from "@/components/info";
+
     
 
 interface ProductPageProps{
@@ -22,11 +28,9 @@ const ProductPage: React.FC<ProductPageProps> = async ({
             <Container>
                 <div className="px-4 py-10 sm:px-6 lg:px-6">
                     <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-                        {/* Galerie */}
-                        <div>galerie</div>
+                        <Gallery images={product.images}/>
                         <div className="mt-10 px-4 sm:mt-16 ms:px-0 lg:mt-0">
-                            {/* infos */}
-                            infos
+                            <Info data={product}/>
                         </div>
                     </div>
                     <hr className="my-10"/>
---
+++ components/info.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { ShoppingCart } from "lucide-react"
+import React from "react"
+
+import { Product } from "@/types"
+
+import Currency from "@/components/ui/currency"
+import Button from "@/components/ui/button"
+
+
+
+interface InfoProps{
+    data: Product
+}
+const Info: React.FC<InfoProps> = ({
+    data
+}) =>{
+    return(
+        <div>
+            <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
+            <div className="mt-3 flex justify-between items-end">
+                <p className="text-2xl text-gray-900 ">
+                    <Currency value={data.price}/>
+                </p>
+            </div>
+            <hr className="my-4"/>
+            <div className="flex flex-col gap-y-6">
+                <div className=" flex items-center gap-x-4">
+                    <h3 className="font-semibold text-black">Taille:</h3>
+                    <div>
+                        {data?.size?.name} 
+                        {/* ou afficher la valeur de la taille */}
+                        {/* {data?.size?.value} */}
+                    </div>
+                </div>
+                <div className=" flex items-center gap-x-4">
+                    <h3 className="font-semibold text-black">Couleur:</h3>
+                    <div 
+                    style = {{backgroundColor: data?.color?.value}}
+                    className="h-6 w-6 rounded-full border border-gray-600"
+                    />
+                </div>
+            </div>
+            <div className="mt-10 flex items-center gap-x-3">
+                <Button className=" flex items-center gap-x-2">
+                    Ajouter au panier
+                    <ShoppingCart/>
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default Info
