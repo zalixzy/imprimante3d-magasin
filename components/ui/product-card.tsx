e1f59e9bb7fe5546b445ff860a219a1d6ffbc2bc
Travier.Thomas <travier.thomas@cci.com>
2024-01-29 13:22:47 +0100

    ajout liaison catégorie avec autres produits suggérés et l'affichage de produit individuel

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import IconButton from "@/components/ui/icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "@/components/ui/currency";
+import { useRouter } from "next/navigation";
 
 
 interface ProductCard{
@@ -13,8 +14,12 @@ interface ProductCard{
 const ProductCard: React.FC<ProductCard> = ({
     data
 }) => {
+    const router = useRouter();
+    const handleClick = () => {
+        router.push(`/product/${data?.id}`)
+    }
     return (
-        <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+        <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
             {/* Images et actions    */}
             <div className="aspect-square rounded-xl bg-gray-100 relative">
                 <Image 
