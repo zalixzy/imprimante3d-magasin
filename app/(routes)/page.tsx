38f8d6cb85ae443bfee658211390fbc7502e2215
Travier.Thomas <travier.thomas@cci.com>
2024-01-29 10:48:59 +0100

    ajout des composants mainnav navbar des interface catégory et panneau d'affichage du container, footer

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,9 @@
+
+
+const HomePage = () =>{
+    return(
+        <div>salut</div>
+    )
+}
+
+export default HomePage
---
+++ components/footer.tsx
@@ -0,0 +1,17 @@
+
+
+const Footer = () => {
+    return(
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-xs text-black">
+                    &copy; 2024 Imprim3d, Tout droit réservés.
+                </p>
+
+            </div>
+         
+        </footer>
+    )
+}
+
+export default Footer;
