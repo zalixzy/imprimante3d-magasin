205ada92362b065b66c683238e76ebc8b624b2f0
Travier.Thomas <travier.thomas@cci.com>
2024-02-04 18:42:49 +0100

    correction typo

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -1,9 +1,8 @@
 
-
 import { Billboard as BillboardType } from "@/types"
 import { ScanFace } from "lucide-react"
 
-interface BillboardProps{
+interface BillboardProps {
     data: BillboardType
 }
 
@@ -12,16 +11,15 @@ const Billboard: React.FC<BillboardProps> = ({
 }) => {
     return (
         <div className="p-4 sm:p-6 rounded-xl overflow:hidden lg:p-6">
-            <div 
-            className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow:hidden bg-cover"
-            style={{ backgroundImage : `url(${data?.imageUrl})` }}>
+            <div
+                className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow:hidden bg-cover"
+                style={{ backgroundImage: `url(${data?.imageUrl})` }}>
                 <div className="h-full w-full text-center items-center flex flex-col justify-center gap-y-8">
                     <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
-                        {data.label}
+                        {data?.label}
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
