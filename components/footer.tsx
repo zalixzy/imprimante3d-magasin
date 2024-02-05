dd29a43a3c32bf0f720f004b620730c40d19ee54
Travier.Thomas <travier.thomas@cci.com>
2024-02-05 13:24:58 +0100

    ajout signature footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="bg-white border-t">
             <div className="mx-auto py-10">
                 <p className="text-center text-xs text-black">
-                    &copy; 2024 Imprim3d, Tout droit réservés.
+                    &copy; 2024 Imprim3d, <a className="text-blue-800 hover:text-blue-400" href="https://zalt.fr">TRAVIER Thomas</a> Tous droit réservés. 
                     <a className="text-blue-800 hover:text-blue-400"  href="https://imprimante3d-admin.vercel.app/"> Portail Administrateur</a>
                 </p>
 
